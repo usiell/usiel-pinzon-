3079b776c589eb9060d7f3cf97a14a6422739425
usiell <60758521+usiell@users.noreply.github.com>
2020-02-06 21:45:30 -0600

    Add files via upload

---
+++ programacion18/ej11.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+
+int n;
+int c;
+int r;
+
+
+return 0;
+}
---
+++ programacion18/ej15.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+
+int n;
+int s;
+int c;
+int p;
+
+return 0;
+}
